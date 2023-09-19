7fc9c93bcb7df7427d3db1edc6ecdbab26ef37c1
ASefedin <115707837+ASefedin@users.noreply.github.com>
2023-09-19 14:47:23 +0200

    Add files via upload

---
+++ ACT2POO_AjdiniSefedin/ACT2POO_AjdiniSefedin/ACT2POO_AjdiniSefedin/Cercle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACT2POO_AjdiniSefedin
+{
+    internal class Cercle
+    {
+        private double _rayon;
+        private double _perimetre;
+        private double _aire;
+
+
+    	public double Rayon
+		{
+			get { return _rayon; }
+			set { _rayon = value; }
+		}
+
+        public double Perimetre
+        {
+            get { return _perimetre; }
+            set { _perimetre = value; }
+        }
+
+
+        public double Aire
+        {
+            get { return _aire; }
+            set { _aire = value; }
+        }
+
+        public void CalculerPerimetre(double rayon)
+        {
+            
+        }
+
+        public void CalculerAire(double rayon)
+        {
+            
+        }
+
+        public string AfficherCaracteristiques()
+        {
+            string chaine = " Le cercle de rayon " + _rayon + " a un perimetre de " + _perimetre + " et une aire de " + _aire;
+            return chaine;
+        }
+
+        public string AfficherCaracteristiquesDivise2()
+        {
+            string chaineDiv2 = " Le cercle de rayon " + _rayon/2 + " a un perimetre de " + _perimetre/2 + " et une aire de " + _aire/2;
+            return chaineDiv2;
+        }
+
+    }
+}
---
+++ ACT2POO_AjdiniSefedin/ACT2POO_AjdiniSefedin/ACT2POO_AjdiniSefedin/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ACT2POO_AjdiniSefedin;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Bienvenu dans ce programme sur le cercle !");
+Console.WriteLine("Tapez un rayon pour votre cercle", out rayon);
+double rayon = double.Parse(Console.ReadLine());
+
+Cercle Cercle1 = new Cercle(rayon, perimetre, aire);
+Console.WriteLine("Avec un ceercle de rayon diminué de moitié : ");
+Console.WriteLine("-----------------------------------------------");
+
+Console.WriteLine("Le cercle de ", rayon/2 , "a un perimetre de ", perimetre/2 , "et une aire de ", aire/2);
+
