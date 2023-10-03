cf9fb516087d45d8c1eb9e3aa26ccfbf71c32f18
ASefedin <115707837+ASefedin@users.noreply.github.com>
2023-10-03 15:53:44 +0200

    Add files via upload

---
+++ ACT2POO_AjdiniSefedin/ACT2POO3_AjdiniSefedin/ACT2POO3_AjdiniSefedin/ACT2POO3_AjdiniSefedin/sandwich.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACT2POO3_AjdiniSefedin
+{
+    internal class sandwich
+    {
+        private string[] _viande = { "jambon", "boeuf", "cochon", "dinde" };
+        private string[] _crudite = { "salade", "carotte", "ognion", "oeuf" };
+        private string[] _pain = { "pain blanc", "pain carre", "pain gris", "pain noir" };
+        private string[] _sauce = { "andalouse", "samourai", "ketchup", "mayo" };
+
+        public int[] Viande
+        {
+            get { return _viande; }
+            set { _viande = value; }
+        }
+
+    }
+}
---
+++ ACT2POO_AjdiniSefedin/ACT2POO_AjdiniSefedin/ACT2POO_AjdiniSefedin/Cercle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACT2POO_AjdiniSefedin
+{
+    internal class Cercle
+    {
+        private double _rayon;
+
+
+    	public double Rayon
+		{
+			get { return _rayon; }
+			set { _rayon = value; }
+		}
+
+
+        public Cercle(double rayon)
+        {
+            _rayon = rayon;
+        }
+
+        public double CalculerPerimetre()
+        {
+            double value = 2 * Math.PI * _rayon;
+            return value;
+        }
+
+        public double CalculerAire()
+        {
+            double value = Math.PI * _rayon * _rayon;
+            return value;
+        }
+
+        public string AfficherCaracteristiques()
+        {
+            string chaine = "Le cercle de rayon " + _rayon + " a un perimetre de " + CalculerPerimetre() + " et une aire de " + CalculerAire();
+            return chaine;
+        }
+
+        public string AfficherCaracteristiquesDivise2()
+        {
+            string chaineDiv2 = "Le cercle de rayon " + _rayon/2 + " a un perimetre de " + CalculerPerimetre () / 2 + " et une aire de " + CalculerAire() / 2;
+            return chaineDiv2;
+        }
+
+    }
+}
